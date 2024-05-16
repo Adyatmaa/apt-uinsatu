4a0bd68fa78778ff504965bd8cef9517a230a1be
roihanfs <41983891+Roihan1303@users.noreply.github.com>
2024-05-16 23:33:43 +0700

    Update Sidebar

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -157,6 +157,7 @@
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
                             <a href="{{ route('pageDataJenjang') }}" class="nav-link">
+                                <i class="far fa-circle nav-icon"></i>
                                 <p>Data Jenjang</p>
                             </a>
                         </li>
